858d8d233e523557fcd6f8fd2d42e2ee6d9d7de5
Peter Lin <41492200+peterlin1@users.noreply.github.com>
2019-08-24 22:10:29 -0400

    Completed 28

---
+++ algorithms/28_Implement_strStr.py
@@ -0,0 +1,51 @@
+class Solution(object):
+
+    def strStr(self, haystack, needle):
+        """
+        Implement strStr().
+
+        Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+        Runtime: 44 ms, faster than 38.07% of Python3 online submissions for Implement strStr().
+        Memory Usage: 13.9 MB, less than 12.31% of Python3 online submissions for Implement strStr().
+
+
+        Parameters
+        ----------
+        haystack : str
+
+        needle : str
+
+
+        Returns
+        -------
+        ret : int
+
+
+        Examples
+        --------
+        >>> Solution().strStr("hello", "ll")
+        2
+
+        >>> Solution().strStr("aaaaa", "bba")
+        -1
+
+        >>> Solution().strStr("mississippi", "issipi")
+        -1
+
+        """
+
+        if len(needle) is 0:
+            return 0
+        for idx, ch in enumerate(haystack):
+            if len(needle) > len(haystack) - idx:
+                return -1
+            pointer = 0
+            while pointer < len(needle):
+                if haystack[idx + pointer] == needle[pointer]:
+                    pointer += 1
+                else:
+                    break
+            if pointer == len(needle):
+                return idx
+        return -1
