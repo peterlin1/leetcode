527dba35dce66a0c1878b1bbde34a586a23dce21
Peter Lin <41492200+peterlin1@users.noreply.github.com>
2019-08-31 21:28:50 -0400

    Completed 199

---
+++ algorithms/199_Binary_Tree_Right_Side_View.py
@@ -0,0 +1,58 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can
+        see ordered from top to bottom.
+
+
+        Parameters
+        ----------
+        root : TreeNode
+
+
+        Returns
+        -------
+        ret : list
+
+        """
+
+        ret = []
+        if root:
+            n_queue = [(root, 0)]
+        else:
+            return ret
+
+        while len(n_queue) > 0:
+            t_root = n_queue.pop(0)
+            try:
+                ret[t_root[1]] = t_root[0].val
+            except IndexError:
+                ret.append(t_root[0].val)
+
+            if t_root[0].left is not None:
+                n_queue.append((t_root[0].left, t_root[1] + 1))
+            if t_root[0].right is not None:
+                n_queue.append((t_root[0].right, t_root[1] + 1))
+
+        return ret
+
+
+if __name__ == "__main__":
+    ex = TreeNode(1)
+    ex.left = TreeNode(2)
+    ex.right = TreeNode(3)
+
+    ex.left.left = TreeNode(5)
+
+    ex.right.left = TreeNode(5)
+    ex.right.right = TreeNode(6)
+
+    print(Solution().rightSideView(ex))
