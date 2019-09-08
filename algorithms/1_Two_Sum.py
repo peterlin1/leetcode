d4b1a05acbddf06a0d4c7d72143a6fb2c4f71368
Peter Lin <41492200+peterlin1@users.noreply.github.com>
2019-09-07 21:16:20 -0400

    Updated 1 with O(n) solution.

--- algorithms/1_Two_Sum.py
+++ algorithms/1_Two_Sum.py
@@ -6,9 +6,13 @@ class Solution:
 
         You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
+        Solution 1
         Runtime: 60 ms, faster than 39.98% of Python3 online submissions for Two Sum.
         Memory Usage: 14.9 MB, less than 12.21% of Python3 online submissions for Two Sum.
 
+        Solution 1
+        Runtime: 52 ms, faster than 96.61% of Python3 online submissions for Two Sum.
+        Memory Usage: 15.3 MB, less than 5.11% of Python3 online submissions for Two Sum.
 
         Parameters
         ----------
@@ -29,32 +33,40 @@ class Solution:
         ret : list
 
         """
-        enum_nums = list(enumerate(nums))
-        enum_nums.sort(key=lambda x: x[1])
-        enum_nums_search = enum_nums.copy()
+        # enum_nums = list(enumerate(nums))
+        # enum_nums.sort(key=lambda x: x[1])
+        # enum_nums_search = enum_nums.copy()
+        #
+        # # print(enum_nums)
+        # ret = list()
+        # for idx, enum_num in enum_nums:
+        #     # print("""idx: {}  num: {}""".format(idx, enum_num))
+        #     enum_nums_search.pop(0)
+        #     # print("""Searching for {} in {}""".format(target - enum_num, enum_nums_search))
+        #     idy = next((v[0] for i, v in enumerate(enum_nums_search) if v[1] == target - enum_num), None)
+        #     if idy is None:
+        #         continue
+        #     # print("""Found idy {}""".format(idy))
+        #
+        #     ret = [idx, idy]
+        #     ret.sort()
+        #     break
+        #
+        # return ret
 
-        # print(enum_nums)
-        ret = list()
-        for idx, enum_num in enum_nums:
-            # print("""idx: {}  num: {}""".format(idx, enum_num))
-            enum_nums_search.pop(0)
-            # print("""Searching for {} in {}""".format(target - enum_num, enum_nums_search))
-            idy = next((v[0] for i, v in enumerate(enum_nums_search) if v[1] == target - enum_num), None)
-            if idy is None:
-                continue
-            # print("""Found idy {}""".format(idy))
-
-            ret = [idx, idy]
-            ret.sort()
-            break
-
-        return ret
+        mem = {}
+        for idx in range(len(nums)):
+            try:
+                return [mem[nums[idx]], idx]
+            except KeyError:
+                mem[target - nums[idx]] = idx
+        return []
 
 
 if __name__ == "__main__":
     # [0,1]
-    Solution().twoSum(nums=[2, 7, 11, 15], target=9)
+    print(Solution().twoSum(nums=[2, 7, 11, 15], target=9))
     # [2,3]
-    Solution().twoSum(nums=[8, 2, 11, 7, 15], target=18)
+    print(Solution().twoSum(nums=[8, 2, 11, 7, 15], target=18))
     # [0,1]
-    Solution().twoSum(nums=[3, 3], target=6)
+    print(Solution().twoSum(nums=[3, 3], target=6))
