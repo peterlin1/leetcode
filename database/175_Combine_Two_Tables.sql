c1ca62e0cbacfbb30d680cc92897be744e59cd16
Peter Lin <41492200+peterlin1@users.noreply.github.com>
2019-11-03 20:13:15 -0500

    Added runtime metrics

--- database/175_Combine_Two_Tables.sql
+++ database/175_Combine_Two_Tables.sql
@@ -1,5 +1,10 @@
---Write a SQL query for a report that provides the following information for each person in the Person table, regardless
---if there is an address for each of those people
+/*
+Write a SQL query for a report that provides the following information for each person in the Person table, regardless
+if there is an address for each of those people
+
+Runtime: 580 ms, faster than 94.41% of Oracle online submissions for Combine Two Tables.
+Memory Usage: 0B, less than 100.00% of Oracle online submissions for Combine Two Tables.
+*/
 
 SELECT
     p.FirstName,
--- database/176_Second_Highest_Salary.sql
+++ database/176_Second_Highest_Salary.sql
@@ -1,4 +1,9 @@
---Write a SQL query to get the second highest salary from the Employee table.
+/*
+Write a SQL query to get the second highest salary from the Employee table.
+
+Runtime: 696 ms, faster than 58.38% of Oracle online submissions for Second Highest Salary.
+Memory Usage: 0B, less than 100.00% of Oracle online submissions for Second Highest Salary.
+*/
 
 SELECT
     max(e.Salary) as SecondHighestSalary
