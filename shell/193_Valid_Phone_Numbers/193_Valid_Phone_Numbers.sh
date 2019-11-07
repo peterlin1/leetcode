2a57773baeee0a3649a3b7a09aeb55a9d95f5b40
Peter Lin <41492200+peterlin1@users.noreply.github.com>
2019-11-06 23:25:48 -0500

    Added runtime to 193

--- shell/193_Valid_Phone_Numbers/193_Valid_Phone_Numbers.sh
+++ shell/193_Valid_Phone_Numbers/193_Valid_Phone_Numbers.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+# Runtime: 8 ms, faster than 25.01% of Bash online submissions for Valid Phone Numbers.
+# Memory Usage: 3.1 MB, less than 75.00% of Bash online submissions for Valid Phone Numbers.
 grep -E '^(\([0-9]{3}\)\s|[0-9]{3}-)[0-9]{3}-[0-9]{4}$' file.txt
