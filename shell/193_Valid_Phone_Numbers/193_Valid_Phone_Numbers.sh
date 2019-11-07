04622be89bbfd3e0f0e096bde99a12e17495fc55
Peter Lin <41492200+peterlin1@users.noreply.github.com>
2019-11-06 23:39:11 -0500

    Completed 195

--- shell/193_Valid_Phone_Numbers/193_Valid_Phone_Numbers.sh
+++ shell/193_Valid_Phone_Numbers/193_Valid_Phone_Numbers.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# Given a text file file.txt that contains list of phone numbers (one per line), write a one liner bash script to print
+# all valid phone numbers.
+
+# You may assume that a valid phone number must appear in one of the following two formats: (xxx) xxx-xxxx or
+# xxx-xxx-xxxx. (x means a digit)
+
+# You may also assume each line in the text file must not contain leading or trailing white spaces.
+
 # Runtime: 8 ms, faster than 25.01% of Bash online submissions for Valid Phone Numbers.
 # Memory Usage: 3.1 MB, less than 75.00% of Bash online submissions for Valid Phone Numbers.
+
 grep -E '^(\([0-9]{3}\)\s|[0-9]{3}-)[0-9]{3}-[0-9]{4}$' file.txt
---
+++ shell/195_Tenth_Line/195_Tenth_Line.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Given a text file file.txt, print just the 10th line of the file.
+
+# Runtime: 0 ms, faster than 100.00% of Bash online submissions for Tenth Line.
+# Memory Usage: 3.7 MB, less than 76.19% of Bash online submissions for Tenth Line.
+
+sed -n 10p < file.txt
