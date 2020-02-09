f297d2ac18de83b50f5639d194762cffed261986
Peter Lin <41492200+peterlin1@users.noreply.github.com>
2020-02-08 21:38:43 -0500

    Improved 463

--- algorithms/463_Island_Perimeter.py
+++ algorithms/463_Island_Perimeter.py
@@ -11,8 +11,8 @@ class Solution(object):
         a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter
         of the island.
 
-        Runtime: 552 ms, faster than 59.49% of Python3 online submissions for Island Perimeter.
-        Memory Usage: 12.9 MB, less than 100.00% of Python3 online submissions for Island Perimeter.
+        Runtime: 528 ms, faster than 75.08% of Python3 online submissions for Island Perimeter.
+        Memory Usage: 12.7 MB, less than 100.00% of Python3 online submissions for Island Perimeter.
 
 
         Parameters
@@ -38,14 +38,20 @@ class Solution(object):
         def _fencing(x_idx, y_idx):
             if x_idx is -1 or y_idx is -1:
                 return 0
-            try:
-                return grid[x_idx][y_idx]
-            except IndexError:
-                return 0
+            if grid[x_idx][y_idx] is 1:
+                return 2
+            return 0
 
         ret = 0
         for x_idx in range(len(grid)):
             for y_idx in range(len(grid[0])):
                 if grid[x_idx][y_idx] is 1:
-                    ret += 4 - _fencing(x_idx - 1, y_idx) - _fencing(x_idx + 1, y_idx) - _fencing(x_idx, y_idx - 1) - _fencing(x_idx, y_idx + 1)
+                    ret += 4 - _fencing(x_idx - 1, y_idx) - _fencing(x_idx, y_idx - 1)
         return ret
+
+
+if __name__ == "__main__":
+    print(Solution().islandPerimeter([[0, 1, 0, 0],
+                                     [1, 1, 1, 0],
+                                     [0, 1, 0, 0],
+                                     [1, 1, 0, 0]]))
