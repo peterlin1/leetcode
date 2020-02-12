86f97ee915624a2c716092b940e7fcf2115e4df8
Peter Lin <41492200+peterlin1@users.noreply.github.com>
2020-02-11 19:15:05 -0500

    Initiated 506

---
+++ algorithms/506_Relative_Ranks.py
@@ -0,0 +1,35 @@
+class Solution(object):
+
+    def findRelativeRanks(self, nums):
+        """
+        Given scores of N athletes, find their relative ranks and the people with the top three highest scores, who will
+        be awarded medals: "Gold Medal", "Silver Medal" and "Bronze Medal".
+
+        Note:
+        N is a positive integer and won't exceed 10,000.
+        All the scores of athletes are guaranteed to be unique.
+
+
+        Parameters
+        ----------
+        nums : list
+
+
+        Returns
+        -------
+        ret : list
+
+
+        Examples
+        --------
+        >>> Solution().findRelativeRanks([5, 4, 3, 2, 1])
+        ["Gold Medal", "Silver Medal", "Bronze Medal", "4", "5"]
+
+        """
+
+        pass
+
+
+if __name__ == "__main__":
+    Solution().findRelativeRanks([5, 4, 3, 2, 1])
+
