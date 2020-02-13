16523120d828a9c1f487a7fb6931730d5475a943
Peter Lin <41492200+peterlin1@users.noreply.github.com>
2020-02-13 00:25:08 -0500

    Completed 506

--- algorithms/506_Relative_Ranks.py
+++ algorithms/506_Relative_Ranks.py
@@ -9,6 +9,9 @@ class Solution(object):
         N is a positive integer and won't exceed 10,000.
         All the scores of athletes are guaranteed to be unique.
 
+        Runtime: 6304 ms, faster than 5.12% of Python3 online submissions for Relative Ranks.
+        Memory Usage: 13.7 MB, less than 100.00% of Python3 online submissions for Relative Ranks.
+
 
         Parameters
         ----------
@@ -27,9 +30,22 @@ class Solution(object):
 
         """
 
-        pass
+        ret = [0] * len(nums)
 
+        for idx in range(len(nums)):
+            for jdx in range(idx + 1, len(nums)):
+                if nums[jdx] > nums[idx]:
+                    ret[idx] += 1
 
-if __name__ == "__main__":
-    Solution().findRelativeRanks([5, 4, 3, 2, 1])
-
+            for jdx in range(idx - 1, -1, -1):
+                if nums[jdx] > nums[idx]:
+                    ret[idx] += 1
+            if ret[idx] is 0:
+                ret[idx] = 'Gold Medal'
+            elif ret[idx] is 1:
+                ret[idx] = 'Silver Medal'
+            elif ret[idx] is 2:
+                ret[idx] = 'Bronze Medal'
+            else:
+                ret[idx] = str(ret[idx] + 1)
+        return ret
