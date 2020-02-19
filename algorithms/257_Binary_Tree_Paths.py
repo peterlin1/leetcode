7f9f2d5451b44d3aacf34e88f614dc9f4bd0ed5f
Peter Lin <41492200+peterlin1@users.noreply.github.com>
2020-02-19 12:52:35 -0500

    Completed 257

--- algorithms/257_Binary_Tree_Paths.py
+++ algorithms/257_Binary_Tree_Paths.py
@@ -11,6 +11,9 @@ class Solution(object):
         """
         Given a binary tree, return all root-to-leaf paths.
 
+        Runtime: 32 ms, faster than 54.23% of Python3 online submissions for Binary Tree Paths.
+        Memory Usage: 12.7 MB, less than 100.00% of Python3 online submissions for Binary Tree Paths.
+
 
         Parameters
         ----------
@@ -22,7 +25,30 @@ class Solution(object):
         ret : list
 
         """
-        pass
+
+        ret = []
+
+        def _traverse(node, pre):
+            if node.left and node.right:
+                _traverse(node.left, """{}->{}""".format(pre, node.val))
+                _traverse(node.right, """{}->{}""".format(pre, node.val))
+            elif node.left:
+                _traverse(node.left, """{}->{}""".format(pre, node.val))
+            elif node.right:
+                _traverse(node.right, """{}->{}""".format(pre, node.val))
+            else:
+                ret.append("""{}->{}""".format(pre, node.val))
+
+        if not root:
+            return ret
+        if not root.left and not root.right:
+            return [str(root.val)]
+        if root.left:
+            _traverse(root.left, str(root.val))
+        if root.right:
+            _traverse(root.right, str(root.val))
+
+        return ret
 
 
 if __name__ == "__main__":
@@ -34,4 +60,4 @@ if __name__ == "__main__":
     ex.right = b
     a.left = c
 
-    Solution().binaryTreePaths(ex)
+    print(Solution().binaryTreePaths(ex))
