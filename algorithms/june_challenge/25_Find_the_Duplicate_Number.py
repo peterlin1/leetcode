3b39f1cf0a5050f49b8bb81ddc321d1d82c3f6b8
Peter Lin <41492200+peterlin1@users.noreply.github.com>
2020-06-25 21:46:37 -0400

    Completed June Challenge Day 25 == 287

---
+++ algorithms/june_challenge/25_Find_the_Duplicate_Number.py
@@ -0,0 +1,53 @@
+class Solution(object):
+
+    def findDuplicate(self, nums):
+        """
+        Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at
+        least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+
+        Note:
+        You must not modify the array (assume the array is read only).
+        You must use only constant, O(1) extra space.
+        Your runtime complexity should be less than O(n2).
+        There is only one duplicate number in the array, but it could be repeated more than once.
+
+        53 / 53 test cases passed.
+        Status: Accepted
+        Runtime: 84 ms
+        Memory Usage: 16.3 MB
+
+
+        Parameters
+        ----------
+        nums : list
+
+
+        Returns
+        -------
+        ret : int
+
+
+        Examples
+        --------
+        >>> Solution().findDuplicate([1, 3, 4, 2, 2])
+        2
+
+        >>> Solution().findDuplicate([3, 1, 3, 4, 2])
+        3
+
+        """
+
+        if len(nums) < 2:
+            return -1
+
+        slow = nums[0]
+        fast = nums[nums[0]]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+        fast = 0
+        while fast != slow:
+            fast = nums[fast]
+            slow = nums[slow]
+        return slow
