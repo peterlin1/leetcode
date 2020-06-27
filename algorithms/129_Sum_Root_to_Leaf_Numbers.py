38d10d0a95c3a3d0666933783ec6154b57e0bc33
Peter Lin <41492200+peterlin1@users.noreply.github.com>
2020-06-26 21:28:16 -0400

    Completed June Challenge Day 26 == 129

---
+++ algorithms/129_Sum_Root_to_Leaf_Numbers.py
@@ -0,0 +1,65 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def sumNumbers(self, root):
+        """
+        Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+
+        An example is the root-to-leaf path 1->2->3 which represents the number 123.
+
+        Find the total sum of all root-to-leaf numbers.
+
+        Note: A leaf is a node with no children.
+
+        Runtime: 24 ms, faster than 97.00% of Python3 online submissions for Sum Root to Leaf Numbers.
+        Memory Usage: 14.1 MB, less than 11.38% of Python3 online submissions for Sum Root to Leaf Numbers.
+
+
+        Parameters
+        ----------
+        root : TreeNode
+
+
+        Returns
+        -------
+        ret : int
+
+        """
+
+        if not root:
+            return 0
+
+        ret = []
+
+        def _iterate(node, digits):
+            if not node.left and not node.right:
+                ret.append(digits + str(node.val))
+
+            if node.left:
+                _iterate(node.left, digits + str(node.val))
+            if node.right:
+                _iterate(node.right, digits + str(node.val))
+
+        _iterate(root, "")
+        # print(ret)
+        return sum([int(_) for _ in ret])
+
+
+if __name__ == "__main__":
+    ex1 = TreeNode(4)
+    a = TreeNode(9)
+    b = TreeNode(0)
+    c = TreeNode(5)
+    d = TreeNode(1)
+
+    ex1.left = a
+    ex1.right = b
+    a.left = c
+    a.right = d
+    Solution().sumNumbers(ex1)
