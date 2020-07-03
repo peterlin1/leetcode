a9cacb9002ceec3dd8f1d3a9861564be5d247f1e
Peter Lin <41492200+peterlin1@users.noreply.github.com>
2020-07-02 23:37:22 -0400

    Completed July Challenge Day 2

---
+++ algorithms/july_challenge/2_Binary_Tree_Level_Order_Traversal_II.py
@@ -0,0 +1,72 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+
+    def levelOrderBottom(self, root):
+        """
+        Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right,
+        level by level from leaf to root).
+
+        For example:
+        Given binary tree [3,9,20,null,null,15,7],
+
+            3
+           / \
+          9  20
+            /  \
+           15   7
+
+        return its bottom-up level order traversal as:
+        [
+          [15,7],
+          [9,20],
+          [3]
+        ]
+
+
+        34 / 34 test cases passed.
+        Status: Accepted
+        Runtime: 32 ms
+        Memory Usage: 14.6 MB
+
+
+        Parameters
+        ----------
+        root : TreeNode
+
+
+        Returns
+        -------
+        ret : list
+
+        """
+        ret = []
+
+        def _add_hierchial(level, *args):
+            for node in args:
+                if node:
+                    try:
+                        ret[level].extend([node.val])
+                    except IndexError:
+                        ret.insert(level, [node.val])
+                    _add_hierchial(level - 1, node.left, node.right)
+
+        _add_hierchial(-1, root)
+        return ret
+
+
+if __name__ == "__main__":
+    ex = TreeNode(3)
+    ex.left = TreeNode(9)
+    ex.right = TreeNode(20)
+
+    ex.right.left = TreeNode(15)
+    ex.right.right = TreeNode(7)
+
+    print(Solution().levelOrderBottom(ex))
